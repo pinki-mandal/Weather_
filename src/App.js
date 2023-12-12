ce159b61690bb3d4a310878f86733fbc0ea18832
Pinki Mandal <99741856+pinki-mandal@users.noreply.github.com>
2023-12-12 16:02:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,71 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios'
-const App=()=>{
-   const [num,setnum]=useState(3)
-   const [name,setname]=useState()
-   const [moves,setmoves]=useState()
-   useEffect(()=>{
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from "react";
+import './App.css';
 
-       async  function getData(){
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
-    setname(response.data.name)
-    setmoves(response.data.moves.length)
-    }
-   getData();
-   })
-   return(
-   <>
-    <h1>choose the value <span style={{color:"red"}}>{num} value</span></h1>
-    <h1> my name is <span style={{color:"blue"}}> {name}</span></h1>
-    <h1>I have <span style={{color:"red"}}>{moves} value</span></h1>
-            <select value={num}  onChange={(event)=>{
-             setnum(event.target.value)
-             }}>
-               <option value="3">3</option>
-               <option value="5">5</option>
-               <option value="25">25</option>
-               <option value="10">10</option>
-               <option value="1">1</option>
-               </select>
-               </>
-   )
-               
-     
-   }
-export default App
+function App() {
+
+
+  const apiKey = "f56f24967aaf51182d1d4df628297c6d"
+  const [inputCity, setInputCity] = useState("")
+  const [data, setData] = useState({})
+
+
+  const getWetherDetails = (cityName) => {
+    if (!cityName) return
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    axios.get(apiURL).then((res) => {
+      console.log("response", res.data)
+      setData(res.data)
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  }
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value)
+    setInputCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    getWetherDetails(inputCity)
+  }
+
+
+  return (
+    <div className="col-md-12">
+      <div className="wetherBg">
+        <h1 className="heading">Weather App</h1>
+
+        <div className="d-grid gap-3 col-4 mt-4">
+          <input type="text" className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput} />
+          <button className="btn btn-primary" type="button"
+            onClick={handleSearch}
+          >Search</button>
+        </div>
+      </div>
+
+      {Object.keys(data).length > 0 &&
+        <div className="col-md-12 text-center mt-5">
+
+          <div className="shadow rounded wetherResultBox">
+            <img className="weathorIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
+
+            <h5 className="weathorCity">
+              {data?.name}
+            </h5>
+            <h6 className="weathorTemp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+            <h6 className="weatherTEMP">{((data?.weather?.main))}</h6>
+            
+          </div>
+        </div>
+      }
+
+    </div>
+  );
+}
+
+export default App;
